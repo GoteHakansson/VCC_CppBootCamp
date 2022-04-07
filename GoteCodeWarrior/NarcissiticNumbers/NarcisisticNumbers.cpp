dfce9a0a6b1857230804d4bb29394fac50826060
Gote.Hakansson <gote.hakansson@gmail.com>
2022-04-07 23:19:21 +0200

    Added Codewar Testing Code

---
+++ GoteCodeWarrior/NarcissiticNumbers/NarcisisticNumbers.cpp
@@ -0,0 +1,20 @@
+bool narcissistic( int value ){
+  //Code away
+  int InputValue = value;
+  int NRofFigures = 0;
+  
+  while(value != 0) {
+      value = value / 10;
+      NrofFigures++;
+   }
+  value = InputValue
+  
+  int iNums = 12476;
+std::ostringstream os;
+
+os << iNums;
+std::string digits = os.str();
+  
+  
+  
+}
